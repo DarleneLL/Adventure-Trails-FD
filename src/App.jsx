bdd9fa91b55f2db4fd6e08f44cab308197111c03
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-04-09 15:50:51 -0300

    Criação do componente CardTrilha

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import CardTrilha from "./components/CardTrilha"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <CardTrilha/>
     </>
   )
 }
---
+++ src/components/CardTrilha/index.jsx
@@ -0,0 +1,9 @@
+function CardTrilha(){
+    return(
+        <h1>Card Trilha</h1>
+    )
+}
+
+// prop-type
+
+export default CardTrilha;
