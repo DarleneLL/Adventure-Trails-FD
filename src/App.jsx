d9faca752c3c5e880ca5324cfcfc99eb86b80ddf
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-04-09 18:04:50 -0300

    componente recebendo props e propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import CardTrilha from "./components/CardTrilha"
+import CardTrilha from "./components/CardTrilha/index"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const listaTrilhas = [
+    {
+      nomeTrilha: "Trilha da Costa da Lagoa",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 120,
+      trajeto: 4,
+      dificuldade: "iniciante",
+      tipo: "caminhada / trekking",
+      nomeUsuario: "Guilherme",
+      urlImagem: "https://images.pexels.com/photos/10031489/pexels-photo-10031489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+  ]
 
   return (
     <>
-      <CardTrilha/>
+    {
+      listaTrilhas.map((trilha, index) => (
+        <CardTrilha dadosTrilha={trilha} key={index}/>
+      ))
+    }
     </>
+    
   )
+    
 }
 
 export default App
--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,7 +1,27 @@
-function CardTrilha(){
+import * as PropTypes from "prop-types"
+
+function CardTrilha({dadosTrilha}){
     return(
-        <h1>Card Trilha</h1>
+        <>
+            <h1>{dadosTrilha.nomeTrilha}</h1>
+            <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha" />
+        </>
     )
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string,
+        estado: PropTypes.string,
+        duracao: PropTypes.number,
+        trajeto: PropTypes.number,
+        dificuldade: PropTypes.string,
+        tipo: PropTypes.string,
+        nomeUsuario: PropTypes.string,
+        urlImagem: PropTypes.string
+    })
 }
 
 // prop-type
